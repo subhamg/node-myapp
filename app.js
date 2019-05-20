8ae5510228b0d0e31543c5dc6a8b977602a9dbf4
Subham Goyal <subhamiitk@gmail.com>
2019-05-20 10:43:53 +0530

    File system module - both async and sync

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-// OS module
+// File System module
 
-const os = require("os");
+const fs = require("fs");
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
+// const files = fs.readdirSync("./");
 
-console.log(`Total Memory: ${totalMemory}`); //Template string
-console.log(`Free Memory: ${freeMemory}`);
+// console.log(files);
+
+// fs.readdir("./", function(err, files) {
+//   if (err) console.log("Error", err);
+//   else console.log("Result", files);
+// });
+
+fs.readdir("./", (err, files) => {
+  if (err) console.log("Error", err);
+  else console.log("Result", files);
+});
