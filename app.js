a03ab309ca42cd081a68275469ae646c1c5f7708
Subham Goyal <subhamiitk@gmail.com>
2019-05-20 11:10:18 +0530

    Passing Event arguments

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ const EventEmitter = require("events"); //EventEmitter is a class, contains prop
 const emitter = new EventEmitter(); // Here emitter is a object
 
 // Register a listener
-emitter.on("messageLogged", function() {
-  console.log("Listener called");
+emitter.on("messageLogged", arg => {
+  //e, eventArg or arg
+  console.log("Listener called", arg);
 });
 
 // Raise an event
-emitter.emit("messageLogged");
+emitter.emit("messageLogged", { id: 1, url: "http://" });
+// id and url are object argument to the emit method.
 
 //Emit means  Making a noise - produce signalling
+
+/* --------- Exercise ----------- */
+
+// Listen to a logging event
+emitter.on("logging", arg => {
+  console.log("Logging Called", arg);
+});
+// Raise an logging event
+emitter.emit("logging", { data: "message" });
