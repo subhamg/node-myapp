3f7616940317f974839c9911e132e301401e512e
Subham Goyal <subhamiitk@gmail.com>
2019-05-20 11:21:07 +0530

    Extended EventEmitter using class

--- app.js
+++ app.js
@@ -2,25 +2,13 @@
 
 const EventEmitter = require("events"); //EventEmitter is a class, contains property and methods.
 
-const emitter = new EventEmitter(); // Here emitter is a object
+const Logger = require("./logger");
+const logger = new Logger();
 
 // Register a listener
-emitter.on("messageLogged", arg => {
+logger.on("messageLogged", arg => {
   //e, eventArg or arg
   console.log("Listener called", arg);
 });
 
-// Raise an event
-emitter.emit("messageLogged", { id: 1, url: "http://" });
-// id and url are object argument to the emit method.
-
-//Emit means  Making a noise - produce signalling
-
-/* --------- Exercise ----------- */
-
-// Listen to a logging event
-emitter.on("logging", arg => {
-  console.log("Logging Called", arg);
-});
-// Raise an logging event
-emitter.emit("logging", { data: "message" });
+logger.log("message");
